a2c676cae50be185dd769e9ebad5cea08ae86768
sobanjawaid26 <sobanjawaid26@gmail.com>
2021-05-29 10:20:16 -0700

    edge case handled in 540 findSingleElementWhichAppearsOnlyOnce

--- easy/findSingleElementWhichAppearsOnlyOnce.py
+++ easy/findSingleElementWhichAppearsOnlyOnce.py
@@ -3,10 +3,12 @@
 #     print("President {}: {}".format(i + 1, presidents[i]))
 from typing import List
 
-nums = [3, 3, 7, 7, 10, 11, 11]
+nums = [1, 1, 2]
 
-
+if len(nums) == 1:
+    print(nums[0])
+if (len(nums) - 1) % 2 == 0 and nums[len(nums) - 1] != nums[len(nums) - 2]:
+    print(nums[len(nums) - 1])
 for i in range(len(nums) - 1):
     if i % 2 == 0 and nums[i] != nums[i + 1]:
         print(nums[i])
-
