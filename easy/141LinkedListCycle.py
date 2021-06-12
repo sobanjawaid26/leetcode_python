2f84fa52d4638e7ffcb5ba375ca27e1ce66de2d4
sobanjawaid26 <sobanjawaid26@gmail.com>
2021-06-12 08:45:41 -0700

    GFG folder added

---
+++ easy/141LinkedListCycle.py
@@ -0,0 +1,17 @@
+class ListNode:
+    def __init__(self, x):
+        self.val = x
+        self.next = None
+
+
+def hasCycle(self, head: ListNode) -> bool:
+    curr = head
+    if head.next is not None:
+        print(head.value)
+        curr = curr.next
+
+
+## insert
+
+
+## Traverse and print
